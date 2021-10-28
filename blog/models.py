aee27f1e962d11bdd68abc4fd69d588bc0c2ef69
MercyIrene <mercyirene3@gmail.com>
2021-10-28 23:05:01 +0300

    changes by mercy irene

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 
 class Blog(models.Model):
-    pass
+    name = models.CharField(max_length=100, null=True)
+    content = models.TextField(null=True, blank=False)
+    date_added = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self): #double underscore
+        return f"{self.name} : {self.date_added} "
+
+    
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
+
+def home(request):
+    blog = Blog.objects.filter(name = "Mercy Irene")
+    q = blog.query
+    return HttpResponse(q)
 
 # Create your views here.
