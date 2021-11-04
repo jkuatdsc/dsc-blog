174c4ba21b95773bb4b8ea58e63bbf17b88e3538
William Otieno <jimmywilliamotieno@gmail.com>
2021-11-04 20:32:21 +0300

    lesson 3

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 
 # Create your models here.
@@ -11,3 +12,18 @@ class Blog(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.date_added.date()}"
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200, default='')
+    content = models.TextField(default='')
+    date_added = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(default='dp.jpeg', upload_to='profile_pics/')
+
+    def save(self, *args, **kwargs):
+        img = Image.open(self.image.path)
+        if img.height > 1000 or img.width > 1500:
+            output_size = ((500, 500))
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+        super().save(*args, **kwargs)
